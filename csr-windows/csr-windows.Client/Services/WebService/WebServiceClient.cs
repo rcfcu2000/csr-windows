a36c701ee97af4c2623e93c4dd3184abc5347532
青 狄 <977434247@qq.com>
2024-05-28 11:43:28 +0800

    暂存Client初版框架

---
+++ csr-windows/csr-windows.Client/Services/WebService/WebServiceClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csr_windows.Client.Services.WebService
+{
+    internal class WebServiceClient
+    {
+        static SunnyNet syNet = new SunnyNet();
+
+    }
+}
