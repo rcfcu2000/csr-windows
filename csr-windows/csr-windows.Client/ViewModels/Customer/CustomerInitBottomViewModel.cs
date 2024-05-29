e53ee444f6c13dff7ed25b99955372110f6a7845
青 狄 <977434247@qq.com>
2024-05-29 21:06:48 +0800

    提交客服界面的下层按钮，以及ToggleImageButton样式（还有左键单击按钮的东西采用ContextMenu的效果不好，改成Popup试试）

---
+++ csr-windows/csr-windows.Client/ViewModels/Customer/CustomerInitBottomViewModel.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csr_windows.Client.ViewModels.Customer
+{
+    public class CustomerInitBottomViewModel :ObservableRecipient
+    {
+
+        #region Fields
+
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        #region Constructro
+        public CustomerInitBottomViewModel()
+        {
+            
+        }
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
