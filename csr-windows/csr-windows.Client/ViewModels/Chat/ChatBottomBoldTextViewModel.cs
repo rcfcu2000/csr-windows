72aa6c159e39dd3cc3f54b79e88d9769f84f6410
青 狄 <977434247@qq.com>
2024-05-31 14:44:59 +0800

    提交最下面是粗文本的窗体跟vm

---
+++ csr-windows/csr-windows.Client/ViewModels/Chat/ChatBottomBoldTextViewModel.cs
@@ -0,0 +1,47 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csr_windows.Client.ViewModels.Chat
+{
+    public class ChatBottomBoldTextViewModel : ObservableRecipient
+    {
+
+        #region Fields
+
+        private string _content;
+
+
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        #region Constructor
+        public ChatBottomBoldTextViewModel()
+        {
+            
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// 文本内容
+        /// </summary>
+        public string Content
+        {
+            get => _content;
+            set => SetProperty(ref _content, value);
+        }
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
