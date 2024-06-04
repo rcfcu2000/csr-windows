69d9c28822dc2682430e67496ea4712f533804a9
青 狄 <977434247@qq.com>
2024-06-04 15:01:48 +0800

    提交切换商品界面以及viewmodel

---
+++ csr-windows/csr-windows.Client/ViewModels/Chat/ChatSwitchProductsViewModel.cs
@@ -0,0 +1,41 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace csr_windows.Client.ViewModels.Chat
+{
+    public class ChatSwitchProductsViewModel : ObservableRecipient
+    {
+        #region Fields
+
+        #endregion
+
+        #region Commands
+        public ICommand CheckProductCommand { get; set; }
+        #endregion
+
+        #region Constrctor
+        public ChatSwitchProductsViewModel()
+        {
+            CheckProductCommand = new RelayCommand(OnCheckProductCommand);
+        }
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Methods
+        private void OnCheckProductCommand()
+        {
+        }
+
+        #endregion
+    }
+}
