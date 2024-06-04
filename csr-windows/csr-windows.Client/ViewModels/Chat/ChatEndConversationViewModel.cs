c1d590b56af9888e16ccc1db645428b56e4a9ed8
青 狄 <977434247@qq.com>
2024-06-04 15:17:20 +0800

    提交结束对话界面

---
+++ csr-windows/csr-windows.Client/ViewModels/Chat/ChatEndConversationViewModel.cs
@@ -0,0 +1,45 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace csr_windows.Client.ViewModels.Chat
+{
+    public class ChatEndConversationViewModel : ObservableRecipient
+    {
+        #region Fields
+
+        #endregion
+
+        #region Commands
+
+        /// <summary>
+        /// 结束对话
+        /// </summary>
+        public ICommand EndConversationCommand { get; set; }
+        #endregion
+
+        #region Constructor
+        public ChatEndConversationViewModel()
+        {
+            EndConversationCommand = new RelayCommand(OnEndConversationCommand);
+        }
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Methods
+
+        private void OnEndConversationCommand()
+        {
+        }
+        #endregion
+    }
+}
