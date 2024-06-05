99a56b022f6b9214ec1f18a881d6837b8f6de49f
青 狄 <977434247@qq.com>
2024-06-05 11:48:08 +0800

    提交滚动条的自动下降到最后

---
+++ csr-windows/csr-windows.Resources/Extensions/ScrollViewerExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace csr_windows.Resources.Extensions
+{
+    public static class ScrollViewerExtensions
+    {
+        public static readonly DependencyProperty AutoScrollToEndProperty =
+            DependencyProperty.RegisterAttached(
+                "AutoScrollToEnd",
+                typeof(bool),
+                typeof(ScrollViewerExtensions),
+                new PropertyMetadata(false, OnAutoScrollToEndChanged));
+
+        public static bool GetAutoScrollToEnd(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(AutoScrollToEndProperty);
+        }
+
+        public static void SetAutoScrollToEnd(DependencyObject obj, bool value)
+        {
+            obj.SetValue(AutoScrollToEndProperty, value);
+        }
+
+        private static void OnAutoScrollToEndChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is ScrollViewer scrollViewer && e.NewValue is bool autoScroll && autoScroll)
+            {
+                scrollViewer.ScrollChanged += ScrollViewer_ScrollChanged;
+                if (scrollViewer.Content is FrameworkElement content)
+                {
+                    content.SizeChanged += (s, ev) => ScrollToEnd(scrollViewer);
+                }
+            }
+            else if (d is ScrollViewer sv)
+            {
+                sv.ScrollChanged -= ScrollViewer_ScrollChanged;
+            }
+        }
+
+        private static void ScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            if (sender is ScrollViewer scrollViewer && e.ExtentHeightChange > 0)
+            {
+                ScrollToEnd(scrollViewer);
+            }
+        }
+
+        private static void ScrollToEnd(ScrollViewer scrollViewer)
+        {
+            scrollViewer.ScrollToVerticalOffset(scrollViewer.ExtentHeight);
+        }
+    }
+}
