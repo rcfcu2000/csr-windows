29b5837e0506fea449d5629fcde63773273bf28c
青 狄 <977434247@qq.com>
2024-06-13 10:14:00 +0800

    提交ProcessHelper

---
+++ csr-windows/csr-windows.Client/Helper/ProcessHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csr_windows.Client.Helpers
+{
+    public class ProcessHelper
+    {
+        /// <summary>
+        /// 判断是否存在相同的程序
+        /// </summary>
+        /// <returns></returns>
+        public static bool GetIsExistSameProgram()
+        {
+            Process[] proc = Process.GetProcessesByName(Assembly.GetExecutingAssembly().GetName().Name);
+            if (proc.Length > 1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
